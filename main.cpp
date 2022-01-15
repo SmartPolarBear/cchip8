16f4e020f489963ee33b8dd199713e9e4121df23
SmartPolarBear <CleverCoolBear@outlook.com>
2022-01-15 22:46:44 +0800

    feat(main): add command parsing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 
-#include <display/display_window.h>
+#include <argparse/argparse.hpp>
 
-int main()
+#include "display/display_window.h"
+
+using namespace std;
+
+using namespace cchip8::display;
+
+int main(int argc,char **argv)
 {
-	std::cout << "Hello, World!" << std::endl;
+	argparse::ArgumentParser arg_parser{ "cchip8" };
 
+	arg_parser.add_argument("-r", "--rom")
+			.help("the CHIP8 ROM file")
+			.default_value(string{ "" })
+			.implicit_value(string{ "" })
+			.nargs(1);
+
+	arg_parser.add_argument("-vs", "--video-scale")
+			.help("Video scale.")
+			.default_value(10)
+			.implicit_value(10);
+
+	arg_parser.add_argument("-cd", "--cycle-delay")
+			.help("Delay per cycle.")
+			.default_value(3)
+			.implicit_value(3);
+
+	try
+	{
+		arg_parser.parse_args(argc, argv);
+	}
+	catch (const std::runtime_error& err)
+	{
+		cout << err.what() << endl;
+		cout << arg_parser;
+		return 1;
+	}
 
 	return 0;
 }
